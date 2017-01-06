6f03cb5fa4b3c352b466a02c5563a81e8d44960c
akshayanandraut <akshay.raut0303@gmail.com>
2017-01-06 19:09:50 +0530

    SplashScreen Timed and Double press to exit done

--- app/src/main/java/io/github/sidrai97/team_82/MainActivity.java
+++ app/src/main/java/io/github/sidrai97/team_82/MainActivity.java
@@ -2,16 +2,24 @@ package io.github.sidrai97.team_82;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
+    private int backPressed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //turn off title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //request fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
@@ -22,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
                 new DataPoint(4, 6)
         });
         graph.addSeries(series);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        backPressed++;
+        if(backPressed == 2) {
+            super.onBackPressed();
+            finishAffinity();
+
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this,"Press back once again to exit", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- app/src/main/java/io/github/sidrai97/team_82/SplashScreenActivity.java
+++ app/src/main/java/io/github/sidrai97/team_82/SplashScreenActivity.java
@@ -3,13 +3,45 @@ package io.github.sidrai97.team_82;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private Intent intent;
+    private long TIME_DELAY = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //turn off title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //request fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
-        startActivity(intent);
+
+
+         intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+
+
+        Timer buttonTimer = new Timer();
+        buttonTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //myButton.setEnabled(true);
+                        startActivity(intent);
+                    }
+                });
+            }
+        }, TIME_DELAY);
     }
 }
