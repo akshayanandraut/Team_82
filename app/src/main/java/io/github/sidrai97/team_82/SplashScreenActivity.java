1dde98bf5a906460d7b706cedbb642b5ef8c67d6
sidrai97 <siddhantraisgr@gmail.com>
2017-01-06 18:43:20 +0530

    logo added

---
+++ app/src/main/java/io/github/sidrai97/team_82/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package io.github.sidrai97.team_82;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
