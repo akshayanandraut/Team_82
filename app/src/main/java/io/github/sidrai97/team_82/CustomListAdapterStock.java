8303ed23a6c38b9657d5237a5d2c2740a6201abc
akshayanandraut <akshay.raut0303@gmail.com>
2017-01-07 20:31:12 +0530

    Added Json Object of Stocks

---
+++ app/src/main/java/io/github/sidrai97/team_82/CustomListAdapterStock.java
@@ -0,0 +1,43 @@
+package io.github.sidrai97.team_82;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by HP on 07-01-2017.
+ */
+
+public class CustomListAdapterStock  extends ArrayAdapter<String> {
+
+    private final AppCompatActivity context;
+    private final String[] stockName;
+
+
+    public CustomListAdapterStock(AppCompatActivity context, String[] stockName) {
+        super(context, R.layout.display_stocks, stockName);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.stockName=stockName;
+
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.display_stocks, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.stock_name);
+
+
+        txtTitle.setText(stockName[position]);
+
+        return rowView;
+
+    };
+}
--- app/src/main/java/io/github/sidrai97/team_82/MainActivity.java
+++ app/src/main/java/io/github/sidrai97/team_82/MainActivity.java
@@ -8,8 +8,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
-
+import io.github.sidrai97.team_82.*;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
@@ -20,16 +27,55 @@ import com.jjoe64.graphview.series.OnDataPointTapListener;
 import com.jjoe64.graphview.series.PointsGraphSeries;
 import com.jjoe64.graphview.series.Series;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import static android.R.id.list;
 import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class MainActivity extends AppCompatActivity {
     private int backPressed = 0;
 
-
+    ListView list;
+    String[] stockName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.display_list);
+
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            JSONArray jr = new JSONArray("Your json string");
+            JSONObject jb = (JSONObject)jr.getJSONObject(0);
+            JSONArray st = jb.getJSONArray("streets");
+            for(int i=0;i<st.length();i++)
+            {
+                String street = st.getString(i);
+                Log.i("..........",""+street);
+                // loop and add it to array or arraylist
+            }
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        CustomListAdapterStock adapter=new CustomListAdapterStock(this, stockName);
+        list=(ListView)findViewById(R.id.display_stocks_list);
+        list.setAdapter(adapter);
+
+
+      list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String selectedItem= stockName[+position];
+                Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
     }
