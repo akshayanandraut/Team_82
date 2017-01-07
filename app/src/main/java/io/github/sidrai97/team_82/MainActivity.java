c4196d852bd992a61507599b8914de2eeb6c9e4b
akshayanandraut <akshay.raut0303@gmail.com>
2017-01-07 14:43:22 +0530

    minor edits

--- app/src/main/java/io/github/sidrai97/team_82/MainActivity.java
+++ app/src/main/java/io/github/sidrai97/team_82/MainActivity.java
@@ -11,8 +11,14 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.jjoe64.graphview.series.Series;
 
 import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.display_list);
    // /*
         GraphView graph = (GraphView) findViewById(R.id.graph);
+        //GraphView graph1 = (GraphView) findViewById(R.id.graph1);
         LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
@@ -45,10 +52,34 @@ public class MainActivity extends AppCompatActivity {
                 new DataPoint(3, 3),
                 new DataPoint(4, 9)
         });
+
+        //graph2
+     /*   BarGraphSeries<DataPoint> series3 = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        BarGraphSeries<DataPoint> series4 = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 6),
+                new DataPoint(1, 5),
+                new DataPoint(2, 2),
+                new DataPoint(3, 3),
+                new DataPoint(4, 9)
+        });
+*/
+
+
+        //graph1 config
         series1.setColor(Color.RED);
         series1.setDrawDataPoints(true);
         series1.setDataPointsRadius(12);
         series1.setThickness(8);
+        series2.setColor(Color.CYAN);
+        series2.setDrawDataPoints(true);
+        series2.setDataPointsRadius(12);
+        series2.setThickness(8);
 
         // set manual X bounds
         graph.getViewport().setYAxisBoundsManual(true);
@@ -60,12 +91,34 @@ public class MainActivity extends AppCompatActivity {
         graph.getViewport().setMaxX(80);
 
         // enable scaling and scrolling
-        graph.getViewport().setScalable(true);
-        graph.getViewport().setScalableY(true);
+
         graph.getViewport().setScrollable(true); // enables horizontal scrolling
         graph.getViewport().setScrollableY(true); // enables vertical scrolling
         graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
         graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
+
+        //graph1 scroll config
+        // set manual X bounds
+/*        graph1.getViewport().setYAxisBoundsManual(true);
+        graph1.getViewport().setMinY(-150);
+        graph1.getViewport().setMaxY(150);
+
+        graph1.getViewport().setXAxisBoundsManual(true);
+        graph1.getViewport().setMinX(4);
+        graph1.getViewport().setMaxX(80);
+
+        // enable scaling and scrolling
+        graph1.getViewport().setScalable(true);
+        graph1.getViewport().setScalableY(true);
+        graph1.getViewport().setScrollable(true); // enables horizontal scrolling
+        graph1.getViewport().setScrollableY(true); // enables vertical scrolling
+        graph1.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+        graph1.getViewport().setScalableY(true); // enables vertical zooming and scrolling
+
+        //graph2 series config
+        series3.setSpacing(1);
+        series4.setSpacing(10);
+*/
 
         ///custom stroke
        /* Paint paint = new Paint();
@@ -74,9 +127,36 @@ public class MainActivity extends AppCompatActivity {
         paint.setPathEffect(new DashPathEffect(new float[]{8, 50}, 0));
         series1.setCustomPaint(paint);*/
 
+//Listen on Tap
+/*
+        series1.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(MainActivity.this, ""+dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
+
+
+    //Labels
+       // String[] time = {"jan","feb","march","april"};
+       // String[] up_downs  = {"0","5","10","15","20","25","30","35","40","45","50","55","60","65","70","75","80","85","90","95","100"};
+       // StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+       // staticLabelsFormatter.setHorizontalLabels(time);
+       // staticLabelsFormatter.setVerticalLabels(up_downs);
+      //  graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+
+       // series3.setColor(Color.RED);
+
+
+
 
         graph.addSeries(series1);
         graph.addSeries(series2);
+      //  graph1.addSeries(series4);
+        //graph1.addSeries(series3);
+
 
         //*/
 
